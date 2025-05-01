636d3dc520f70e78ca857f65619b89fd4aa631f3
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-05-01 21:31:40 +0300

    checking env

---
+++ src/utils/getEnvVar.js
@@ -0,0 +1,16 @@
+//змінні оточення
+//перевірка наявності зміннної
+
+import dotenv from 'dotenv';
+
+dotenv.config(); //ініціалізація змінних оточення
+
+//env('PORT', '3000')
+export function getEnvVar(name, defaultValue) {
+  const value = process.env[name]; //читання/доступ змінних оточення
+
+  if (value) return value;
+  if (defaultValue) return defaultValue;
+
+  throw new Error(`Missing: process.env[${name}].`);
+}

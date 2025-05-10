ea746a5817e07c98b9a3fab6ac8c577872563b28
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-05-10 18:28:12 +0300

    put ?? validation

--- src/routers/tires.js
+++ src/routers/tires.js
@@ -16,6 +16,18 @@ import { isValidId } from '../middlewares/isValidId.js';
 const router = Router();
 router.use('/:tireId', isValidId('tireId')); //відпрацює скрізь де є шлях :tireId
 
+// // Middleware для вибору схеми валідації для PUT
+// const selectUpsertSchema = async (req, res, next) => {
+//   const { tireId } = req.params;
+//   try {
+//     const tire = await TiresCollection.findById(tireId);
+//     req.validationSchema = tire ? updateTireSchema : createTireSchema;
+//     next();
+//   } catch (error) {
+//     next(createHttpError(500, 'Error checking tire existence'));
+//   }
+// };
+
 router.get('/', ctrlWrapper(getTiresController));
 
 router.get('/:tireId', ctrlWrapper(getTireByIdController));
@@ -36,6 +48,13 @@ router.put(
   validateBody(createTireSchema),
   ctrlWrapper(upsertTireController),
 );
+
+// router.put(
+//   '/:tireId',
+//   selectUpsertSchema,
+//   (req, res, next) => validateBody(req.validationSchema)(req, res, next),
+//   ctrlWrapper(upsertTireController),
+// );
 
 //PATCH
 router.patch(
